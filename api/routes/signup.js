921bf6d7f31231db6164a8d9350e7309b2290109
Rahul Bhate <rbhate@aaigl.com.au>
2019-07-31 14:30:12 +1000

    Express Server Routes changed

--- api/routes/signup.js
+++ api/routes/signup.js
@@ -2,25 +2,54 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../../server/models/user');
+const bcrypt = require('bcrypt');
 
 router.post('/', (req, res, next) => {
   let email = req.body.email;
   let password = req.body.password;
   console.log(email, password);
-
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email: email,
-    password: password,
-  });
-  user
-    .save()
-    .then((result) => {
-      console.log(result);
+  User.find({ email: email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: 'Mail Exists',
+        });
+      } else {
+        bcrypt.hash(password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: 'User Created',
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+            res.status(200).json({ email: email, password: password });
+          }
+        });
+      }
     })
     .catch((err) => {
-      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-  res.status(200).json({ email: email, password: password });
 });
 module.exports = router;
--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true },
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
   password: { type: String, required: true },
 });
 
